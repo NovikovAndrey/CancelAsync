436a1e4e03a7e106ee6f58620f11616994c0361e
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-11 19:38:18 +0300

    Add project files.

---
+++ CancelAsync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CancelAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
